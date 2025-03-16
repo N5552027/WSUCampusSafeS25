e05fb5a87323f0a12690b97c9b3479bb225d6b53
N Mike <noahnicholas.manuel@wsu.edu>
2025-03-16 16:02:06 -0700

    Improved visuals of safety information page.

--- Application/Front End/JScriptFiles/SubmitIncidentReport.js
+++ Application/Front End/JScriptFiles/SubmitIncidentReport.js
@@ -3,13 +3,14 @@ function submitReport() {
     const date = document.getElementById('date').value;
     const time = document.getElementById('time').value;
     const description = document.getElementById('description').value;
+    const location = document.getElementById('location').value;
 
-    if (!title || !date || !time || !description) {
+    if (!title || !date || !time || !location || !description) {
         alert('Please fill out all fields.');
         return;
     }
 
-    const csvData = `Title,Date,Time,Description\n${title},${date},${time},${description}\n`;
+    const csvData = `Title,Date,Time,Location,Description\n${title},${date},${time},${location},${description}\n`;
     const blob = new Blob([csvData], { type: 'text/csv' });
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement('a');
